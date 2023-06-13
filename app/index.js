77b18efcfd3b7aad7d134d29f3c279aa8b61f88a
npmcoding <34496277+npmcoding@users.noreply.github.com>
2023-06-13 16:47:24 -0500

    Add number tumbler and button

--- app/index.js
+++ app/index.js
@@ -1,4 +1,33 @@
 import * as document from "document";
 
-const myLabel = document.getElementById("myLabel");
-myLabel.text = "Hi, Brittany!";
+const tumblerHour = document.getElementById("tumbler-hour");
+const tumblerMins = document.getElementById("tumbler-mins");
+
+const getHour = () => {
+    const selectedIndex = parseInt(tumblerHour.value);
+    const selectedItem = tumblerHour.getElementById(`hour-item${selectedIndex}`);
+    return selectedItem.getElementById("text").text;
+}
+
+const getMinute = () => {
+    const selectedIndex = tumblerMins.value;
+    const selectedItem = tumblerMins.getElementById(`min-item${selectedIndex}`);
+    return selectedItem.getElementById("text").text;
+}
+
+
+tumblerHour.addEventListener("select", (evt) => {
+    console.log(`Hour: ${getHour()}`);
+  });
+  
+  tumblerMins.addEventListener("select", (evt) => {
+    console.log(`Minute: ${getMinute()}`);
+  });
+
+  const startButton = document.getElementById("start-button");
+
+startButton.addEventListener("click", (evt) => {
+  console.log(`${getHour()}:${getMinute()}`);
+
+  console.log("START");
+})
