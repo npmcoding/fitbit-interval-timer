73932c5f2f5ddf09a8e8d3045124e832f9ede894
npmcoding <34496277+npmcoding@users.noreply.github.com>
2023-06-14 17:07:22 -0500

    App and companion are talking

--- app/index.js
+++ app/index.js
@@ -1,33 +1,76 @@
 import * as document from "document";
+import * as messaging from "messaging";
 
 const tumblerHour = document.getElementById("tumbler-hour");
 const tumblerMins = document.getElementById("tumbler-mins");
 
 const getHour = () => {
-    const selectedIndex = parseInt(tumblerHour.value);
-    const selectedItem = tumblerHour.getElementById(`hour-item${selectedIndex}`);
-    return selectedItem.getElementById("text").text;
-}
+  const selectedIndex = parseInt(tumblerHour.value);
+  const selectedItem = tumblerHour.getElementById(`hour-item${selectedIndex}`);
+  return selectedItem.getElementById("text").text;
+};
 
 const getMinute = () => {
-    const selectedIndex = tumblerMins.value;
-    const selectedItem = tumblerMins.getElementById(`min-item${selectedIndex}`);
-    return selectedItem.getElementById("text").text;
-}
-
+  const selectedIndex = tumblerMins.value;
+  const selectedItem = tumblerMins.getElementById(`min-item${selectedIndex}`);
+  return selectedItem.getElementById("text").text;
+};
 
 tumblerHour.addEventListener("select", (evt) => {
-    console.log(`Hour: ${getHour()}`);
-  });
-  
-  tumblerMins.addEventListener("select", (evt) => {
-    console.log(`Minute: ${getMinute()}`);
-  });
+  console.log(`Hour: ${getHour()}`);
+});
 
-  const startButton = document.getElementById("start-button");
+tumblerMins.addEventListener("select", (evt) => {
+  console.log(`Minute: ${getMinute()}`);
+});
+
+
+const fetchTimer = () => {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    // 2
+    messaging.peerSocket.send({
+      command: "fetchTimer",
+    });
+  }
+};
+
+const createTimer = () => {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    messaging.peerSocket.send({
+      command: "createTimer",
+      hour: getHour(),
+      minute: getMinute(),
+    });
+  }
+};
+
+const startButton = document.getElementById("start-button");
 
 startButton.addEventListener("click", (evt) => {
   console.log(`${getHour()}:${getMinute()}`);
 
   console.log("START");
-})
+  createTimer();
+
+});
+
+const setTimer = (timer) => {
+  // 6
+  console.log(`start timer at ${timer}`);
+};
+
+// 1
+messaging.peerSocket.addEventListener("open", (evt) => {
+  fetchTimer();
+});
+
+messaging.peerSocket.addEventListener("message", (evt) => {
+  if (evt.data) {
+    // 5
+    setTimer(JSON.stringify(evt.data));
+  }
+});
+
+messaging.peerSocket.addEventListener("error", (err) => {
+  console.error(`Connection error: ${err.code} - ${err.message}`);
+});
--- companion/index.js
+++ companion/index.js
@@ -1,1 +1,29 @@
-console.log('Hello world!');
+import * as messaging from "messaging";
+
+function returnCurrentTimer(data) {
+    if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+      // 4
+        messaging.peerSocket.send({hour: '1', minute: '23'});
+    } else {
+      console.error("Error: Connection is not open");
+    }
+  }
+  
+  messaging.peerSocket.addEventListener("message", (evt) => {
+    switch (evt.data?.command) {
+        // 3
+        case "fetchTimer": {
+            returnCurrentTimer();
+            break;
+        }
+        case "createTimer": 
+        default: {
+            console.log(evt.data);
+            break;
+        }        
+    }
+  });
+  
+  messaging.peerSocket.addEventListener("error", (err) => {
+    console.error(`Connection error: ${err.code} - ${err.message}`);
+  });
